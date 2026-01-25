e46a2dcffcb4f0a1f7acf4e1a60c6cdbd7125375
Arpit Jain <sachin.arpit.learning@gmail.com>
2026-01-25 22:32:02 +0530

    Fixed CORS

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
 from routes import jobs, status, dlq, health
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="Doc-Transcribe API",
-    version="1.0.0"
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://doc-transcribe-ui.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 app.include_router(jobs.router)
 app.include_router(status.router)
