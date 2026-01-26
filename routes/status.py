e2a486668cca9c8c687bf0ffd1c310ada2dd8297
Arpit Jain <sachin.arpit.learning@gmail.com>
2026-01-26 19:49:26 +0530

    GCS changes - download o/p file on local

--- routes/status.py
+++ routes/status.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
+
+from fastapi.responses import RedirectResponse
+from fastapi import HTTPException
 from services.redis_client import redis_client
 from services.gcs import generate_signed_url
 
@@ -13,7 +16,6 @@ def get_job_status(job_id: str):
 
     return {k: v for k, v in data.items()}
 
-
 @router.get("/{job_id}/output")
 def download_output(job_id: str):
     data = redis_client.hgetall(f"job_status:{job_id}")
@@ -22,14 +24,11 @@ def download_output(job_id: str):
         raise HTTPException(404, "Job not found")
 
     gcs_uri = data.get("output_path")
-
     if not gcs_uri or not gcs_uri.startswith("gs://"):
         raise HTTPException(404, "Output not available")
 
-    download_url = generate_signed_url(gcs_uri)
+    signed_url = generate_signed_url(gcs_uri, expires_minutes=15)
 
-    return {
-        "job_id": job_id,
-        "download_url": download_url
-    }
+    # 🔑 browser will download automatically
+    return RedirectResponse(url=signed_url, status_code=302)
 
