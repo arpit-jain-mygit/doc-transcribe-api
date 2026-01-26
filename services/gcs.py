c1421cbbf2b2bc2bbf832603baeb1cfcaa06ac87
Arpit Jain <sachin.arpit.learning@gmail.com>
2026-01-26 19:53:42 +0530

    GCS changes - download o/p file on local

--- services/gcs.py
+++ services/gcs.py
@@ -33,14 +33,13 @@ def _get_client():
 
 
 def generate_signed_url(gcs_uri: str, expires_minutes: int = 15) -> str:
-    """
-    Generate a temporary HTTPS download URL for a GCS object.
-    """
     if not gcs_uri.startswith("gs://"):
         raise ValueError("Invalid GCS URI")
 
     _, rest = gcs_uri.split("gs://", 1)
     bucket_name, blob_name = rest.split("/", 1)
+
+    filename = blob_name.split("/")[-1]
 
     client = _get_client()
     bucket = client.bucket(bucket_name)
@@ -50,4 +49,7 @@ def generate_signed_url(gcs_uri: str, expires_minutes: int = 15) -> str:
         version="v4",
         expiration=timedelta(minutes=expires_minutes),
         method="GET",
+        response_disposition=f'attachment; filename="{filename}"',
+        response_type="text/plain",
     )
+
