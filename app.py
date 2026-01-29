00f45cd39e6022262c73c13eecb63658a1da8d49
Arpit Jain <sachin.arpit.learning@gmail.com>
2026-01-29 22:19:34 +0530

    UI URL changed for CORS

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://doc-transcribe-ui.vercel.app",
+        "https://doc-transcribe.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
